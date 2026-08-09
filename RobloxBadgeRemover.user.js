07f7361de02fa9208678943d9319e42bbc45361f
T3CHCSS <134118565+T3CHCSS@users.noreply.github.com>
2026-08-08 22:34:42 -0400

    Update RobloxBadgeRemover.user.js

--- RobloxBadgeRemover.user.js
+++ RobloxBadgeRemover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Badge Remover V3
 // @namespace    https://github.com/T3CHCSS/RobloxBadgeRemover/
-// @version      0.35.45
+// @version      0.35.46
 // @description  Removes selected Roblox badges from your account.
 // @author       Seanszy
 // @homepageURL  https://github.com/T3CHCSS/RobloxBadgeRemover/
